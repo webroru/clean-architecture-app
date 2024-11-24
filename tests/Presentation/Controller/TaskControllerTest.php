b591a9bc4d207e201474ff2c0dd80e2d9c2edac4
Igor Nemykin <webroru@gmail.com>
2024-11-24 14:00:07 +0100

    update TaskControllerTest

--- tests/Presentation/Controller/TaskControllerTest.php
+++ tests/Presentation/Controller/TaskControllerTest.php
@@ -23,4 +23,24 @@ class TaskControllerTest extends WebTestCase
         $this->assertIsArray($tasks);
         $this->assertCount(0, $tasks);
     }
+
+    public function testCreateTask(): void
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/tasks', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode([
+            'title' => 'New Task',
+            'description' => 'Task Description',
+        ]));
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('id', $response);
+        $this->assertSame('New Task', $response['title']);
+        $this->assertSame('Task Description', $response['description']);
+    }
 }
